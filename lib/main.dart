45f386a324eb63987ec443725da97b484bace686
sebastien.berruer <sebastien.berruer@adventiel.fr>
2019-11-22 14:58:15 +0100

    refactor: remove root widget and replace it with custom app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mylibrary/MyLibraryApp.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MyLibraryApp(
-    );
-  }
-}
-
-
+void main() => runApp(MyLibraryApp());
